216c9569aa404f3c6810dd8ea46a54388c92d96e
LinZhaoqian <1203783169@qq.com>
2021-01-19 01:24:32 +0800

    完成了基础的移动

---
+++ SuperCold/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 1000f;
+    public Transform movePoint;
+    public int gridWidth = 64 ;
+    
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //使player向MovePonit的位置移动
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, movePoint.position, 5);//还是搞不清楚这个函数，大概是用来从一个位置移动到另一个位置的，但是不知道最后一个空应该填什么
+        }
+
+        //四个方向的面向
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            movePoint.position = new Vector3(-gridWidth, -gridWidth / 2, 0f) + transform.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            movePoint.position = new Vector3(gridWidth, gridWidth / 2, 0f) + transform.position;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            movePoint.position = new Vector3(-gridWidth, gridWidth / 2, 0f) + transform.position;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            movePoint.position = new Vector3(gridWidth, -gridWidth / 2, 0f) + transform.position;
+        }
+    }
+
+}
